1ee52d5095fa333a954d3983513c39f21055d8fa
Florian <escande.florian.chatco@gmail.com>
2023-09-12 19:15:40 +0200

    indexeur-media

---
+++ ConsoleApp1/Library.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp1;
+
+public class Library
+{
+    private List<Media> mediaCollection;
+
+    public Library()
+    {
+        mediaCollection = new List<Media>();
+    }
+    
+    public Library(List<Media> mediaCollection)
+    {
+        this.mediaCollection = mediaCollection;
+    }
+
+    public void AddMedia(Media media)
+    {
+        mediaCollection.Add(media);
+    }
+
+    public void RemoveMedia(Media media)
+    {
+        mediaCollection.Remove(media);
+    }
+
+    public List<Media> GetAllMedia()
+    {
+        return mediaCollection;
+    }
+
+    public Media this[int numeroReference]
+    {
+        get
+        {
+            Media media = mediaCollection.Find(m => m.NumeroReference == numeroReference);
+            if (media == null)
+            {
+                throw new InvalidOperationException($"Aucun média trouvé avec le numéro de référence {numeroReference}");
+            }
+            return media;
+        }
+    }
+}
